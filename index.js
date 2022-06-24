3ba8547dbdbbd4ff301c507ae86842abed0301da
MSCarneiro86 <marcelocarneiro@outlook.com>
2022-06-24 18:03:38 -0300

    First Committ

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ setInterval(async () => {
     const Binance = await kraken.fetchOrderBook ('ETHUSDT')
     const Ftx = await kraken.fetchOrderBook ('ETHUSDT')
     const Coinbase = await kraken.fetchOrderBook ('ETHUSDT')
-    const Kraken = await kraken.fetchOrderBook ('ETHUSDT')
     const Kucoin = await kraken.fetchOrderBook ('ETHUSDT')
     const Gateio = await kraken.fetchOrderBook ('ETHUSDT')
     const Bitfinex = await kraken.fetchOrderBook ('ETHUSDT')
-    const Huobi = await kraken.fetchOrderBook ('ETHUSDT')
+
 
     console.log (`Binance`)
     console.log (`Venda: ${Binance.bids[0][0]}`)
@@ -37,11 +36,6 @@ setInterval(async () => {
     console.log (`Compra: ${Coinbase.asks[0][0]}`)
     console.log (`Compra:${Coinbase.timestamp} \n`)
 
-    console.log (`Kraken`)
-    console.log (`Venda: ${Kraken.bids[0][0]}`)
-    console.log (`Compra: ${Kraken.asks[0][0]}`)
-    console.log (`Compra:${Kraken.timestamp} \n`)
-
     console.log (`Kucoin`)
     console.log (`Venda: ${Kucoin.bids[0][0]}`)
     console.log (`Compra: ${Kucoin.asks[0][0]}`)
@@ -57,9 +51,6 @@ setInterval(async () => {
     console.log (`Compra: ${Bitfinex.asks[0][0]}`)
     console.log (`Compra:${Bitfinex.timestamp} \n`)
 
-    console.log (`Huobi`)
-    console.log (`Venda: ${Huobi.bids[0][0]}`)
-    console.log (`Compra: ${Huobi.asks[0][0]}`)
-    console.log (`Compra:${Huobi.timestamp} \n`)
+
       
 }, 3000)
