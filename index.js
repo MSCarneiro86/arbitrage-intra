3cfb5057bc84fb7fcde843fe7630943a5e3b3849
MSCarneiro86 <marcelocarneiro@outlook.com>
2022-06-28 14:31:44 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,56 +1,22 @@
-'use strict';
-const ccxt = require ('ccxt');
-
-let binance    = new ccxt.binance ()
-let ftx  = new ccxt.ftx ({ verbose: true })
-let coinbase  = new ccxt.coinbase ()
-let kraken = new ccxt.kraken ()
-let kucoin = new ccxt.kucoin ()
-let gateio = new ccxt.gateio ()
-let bitfinex = new ccxt.bitfinex ()
-let huobi = new ccxt.huobi ()
+const Binance = require('./exchanges/binance');
+const Kraken = require('./exchanges/kraken');
+const Bitfinex = require('./exchanges/bitfinex');
+const Ftx = require('./exchanges/ftx');
+const Coinbase = require('./exchanges/coinbase');
+const Kucoin = require('./exchanges/kucoin');
+const gateio = require('./exchanges/gateio');
+const huobi = require('./exchanges/huobi');
 
 setInterval(async () => {
-   
+    
+    Binance.BinanceAPI();
+    Kraken.KrakenAPI();
+    Bitfinex.BitfinexAPI();
+    Ftx.FtxAPI();
+    Coinbase.CoinbaseAPI();
+    Kucoin.KucoinAPI();
+    gateio.GateioAPI();
+    huobi.HuobiAPI(); 
+    
 
-    const Binance = await kraken.fetchOrderBook ('ETHUSDT')
-    const Ftx = await kraken.fetchOrderBook ('ETHUSDT')
-    const Coinbase = await kraken.fetchOrderBook ('ETHUSDT')
-    const Kucoin = await kraken.fetchOrderBook ('ETHUSDT')
-    const Gateio = await kraken.fetchOrderBook ('ETHUSDT')
-    const Bitfinex = await kraken.fetchOrderBook ('ETHUSDT')
-
-
-    console.log (`Binance`)
-    console.log (`Venda: ${Binance.bids[0][0]}`)
-    console.log (`Compra: ${Binance.asks[0][0]}`) 
-    console.log (`timestamp:${Binance.timestamp} \n`)
-
-    console.log (`Ftx`)
-    console.log (`Venda: ${Ftx.bids[0][0]}`)
-    console.log (`Compra: ${Ftx.asks[0][0]}`)
-    console.log (`timestamp:${Ftx.timestamp} \n`)
-
-    console.log (`Coinbase`)
-    console.log (`Venda: ${Coinbase.bids[0][0]}`)
-    console.log (`Compra: ${Coinbase.asks[0][0]}`)
-    console.log (`timestamp:${Coinbase.timestamp} \n`)
-
-    console.log (`Kucoin`)
-    console.log (`Venda: ${Kucoin.bids[0][0]}`)
-    console.log (`Compra: ${Kucoin.asks[0][0]}`)
-    console.log (`timestamp:${Kucoin.timestamp} \n`)
-
-    console.log (`Gateio`)
-    console.log (`Venda: ${Gateio.bids[0][0]}`)
-    console.log (`Compra: ${Gateio.asks[0][0]}`)
-    console.log (`timestamp:${Gateio.timestamp} \n`)
-
-    console.log (`Bitfinex`)
-    console.log (`Venda: ${Bitfinex.bids[0][0]}`)
-    console.log (`Compra: ${Bitfinex.asks[0][0]}`)
-    console.log (`timestamp:${Bitfinex.timestamp} \n`)
-
-
-      
-}, 3000)
+}, process.env.CRAWLER_INTERVAL );
